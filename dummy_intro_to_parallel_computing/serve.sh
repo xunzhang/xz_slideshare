f1963e61879af2481663688726fcb506d4d01e7f
xunzhang <xunzhangthu@gmail.com>
2013-10-15 19:25:52 +0800

    first version of series1.html finished.

--- dummy_intro_to_parallel_computing/serve.sh
+++ dummy_intro_to_parallel_computing/serve.sh
@@ -19,4 +19,4 @@ else
   open=xdg-open
 fi
 
-$open http://localhost:$port/template.html && python -m SimpleHTTPServer $port;
+$open http://localhost:$port/series1.html && python -m SimpleHTTPServer $port;
